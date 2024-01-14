fa0f230d4ad2413e6c05e86a5151f4cc46c48674
Dardo Altobelli <altobellidardo@gmail.com>
2024-01-14 10:54:46 -0300

    refactor optimal accordion styles

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -4,21 +4,19 @@ import Button from './ui/Button'
 function AccordionItem ({ part }) {
   const { title, description, url } = part
   return (
-    <div className='border-2 border-orange-300 rounded-xl mb-4 overflow-hidden'>
-      <details name='fce'>
-        <summary className='hover:underline underline-offset-4 hover:text-orange-500 bg-orange-300 p-4'>
-          {title}
-        </summary>
-        <div className='flex flex-col gap-[20px] p-4'>
-          <p>{description}</p>
-          <Link href={url}>
-            <Button>
-              Test
-            </Button>
-          </Link>
-        </div>
-      </details>
-    </div>
+    <details name='fce' className='border-2 border-orange-300 rounded-xl mb-2 overflow-hidden'>
+      <summary className='hover:underline underline-offset-4 hover:text-orange-500 bg-orange-300 p-4'>
+        {title}
+      </summary>
+      <div className='flex flex-col gap-[20px] p-4'>
+        <p>{description}</p>
+        <Link href={url}>
+          <Button>
+            Test
+          </Button>
+        </Link>
+      </div>
+    </details>
   )
 }
 
